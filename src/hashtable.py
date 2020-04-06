030f1b89001d1d83f20a84a49fac8348c74fd961
apathyhill <apathyhill@gmail.com>
2020-04-06 18:13:11 -0400

    Assignment - 2020/04/06

--- src/hashtable.py
+++ src/hashtable.py
@@ -44,50 +44,63 @@ class HashTable:
 
 
     def insert(self, key, value):
-        '''
-        Store the value with the given key.
+        index = self._hash_mod(key) # Get index of key's hash
 
-        # Part 1: Hash collisions should be handled with an error warning. (Think about and
-        # investigate the impact this will have on the tests)
+        if not self.retrieve(key) == None: # If key in storage, remove it
+            self.remove(key)
 
-        # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
-
-        Fill this in.
-        '''
-        pass
-
+        if self.storage[index] == None: # If empty slot, insert value
+            self.storage[index] = LinkedPair(key, value)
+        else: # Otherwise, go through all nodes until there's no next node
+            node = self.storage[index]
+            while node.next:
+                node = node.next
+            node.next = LinkedPair(key, value) # Insert at end of the last node
 
 
     def remove(self, key):
-        '''
-        Remove the value stored with the given key.
-
-        Print a warning if the key is not found.
-
-        Fill this in.
-        '''
-        pass
+        index = self._hash_mod(key) # Get index of key's hash
+        if self.storage[index] == None: # Can't remove empty index
+            return
+        else: # Go through all nodes
+            node = self.storage[index]
+            node_prev = None
+            while node:
+                if node.key == key: # If found key
+                    if node_prev: # Set previous node's next to this node's next
+                        node_prev.next = node.next
+                    else:
+                        self.storage[index] = None # Otherwise, remove it enitrely
+                    return
+                node_prev = node
+                node = node.next
 
 
     def retrieve(self, key):
-        '''
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Fill this in.
-        '''
-        pass
+        index = self._hash_mod(key)  # Get index of key's hash
+        if self.storage[index] == None: # If empty index, return None
+            return None
+        else:
+            node = self.storage[index] # Go through all nodes
+            while node:
+                if node.key == key:
+                    return node.value # Return value if found
+                node = node.next
+        return None
 
 
     def resize(self):
-        '''
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
-
-        Fill this in.
-        '''
-        pass
+        self.capacity *= 2 # Double capacity
+        temp = [] # Keep temp array with key-value pairs
+        for i in self.storage:
+            if not i == None:
+                node = i 
+                while node:
+                    temp.append((node.key, node.value)) # Add pairs to array
+                    node = node.next
+        self.storage = [None] * self.capacity # Make new storage array
+        for pair in temp:
+            self.insert(pair[0], pair[1])  # Insert all the pairs back in
 
 
 
